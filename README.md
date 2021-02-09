a9219dd78efe55cfb94810c3f55000fc95d294e1
Senol Tas <senol@solutas.ch>
2021-02-10 00:49:52 +0100

    initial install and login

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
 # soluneo-cli
-soluneo cli 
+
+soluneo cli
 
 yarn install
+yarn link
+
+# local development server
+
+right now only one is working at one machine until $rand function is implemented.
+
+- ```sol login```
+  - somewhere for global login
+- ```sol install```
+  - in directory you want to isntall the dev server (should be another dierctory then application code)
+- ```sol start```
+  - in installation directory
+- ```sol stop```
+  - in installation directory
+
+right now it is starting on port 80 - will be configurable in future.
 
 # usage
+
+in directory with application code (first download some app from your instance or try https://github.com/solutas/solutas.ch as an example)
+
 ```
-sol --username *** --password **** --server http://localhost 
+sol --username *** --password **** --server http://localhost
 ```
+
 will start watch mode
--- src/cli.js
+++ src/cli.js
@@ -4,12 +4,12 @@
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
  * this file except in compliance with the License. You may obtain a copy of the
  * License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
  * WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
  * MERCHANTABLITY OR NON-INFRINGEMENT.
- * 
+ *
  * See the Apache Version 2.0 License for specific language governing permissions
  * and limitations under the License.
  ******************************************************************************/
@@ -25,13 +25,23 @@ import zip from "bestzip";
 import ora from "ora";
 import fetch from "node-fetch";
 import chalk from "chalk";
-import FormData from 'form-data';
+import FormData from "form-data";
+import express from "express";
+import open from "open";
+import cors from "cors";
+import bodyParser from "body-parser";
+const extract = require('extract-zip')
+
+const { Observable } = require("rxjs");
+const execa = require('execa');
+
 
 const mkdtemp = util.promisify(fs.mkdtemp);
 
 function parseArgumentsIntoOptions(rawArgs) {
   const args = arg(
     {
+      "--login": String,
       "--username": String,
       "--password": String,
       "--server": String,
@@ -46,15 +56,16 @@ function parseArgumentsIntoOptions(rawArgs) {
     }
   );
   return {
+    login: args["--login"] || false,
     skipPrompts: args["--yes"] || false,
     git: args["--git"] || false,
     username: args["--username"] || false,
     password: args["--password"]
       ? args["--password"]
-          //.split("")
-         // .map((k) => "*")
-         // .join("")
-      : false,
+      : //.split("")
+        // .map((k) => "*")
+        // .join("")
+        false,
     server: args["--server"] || false,
     template: args._[0],
     runInstall: args["--install"] || false,
@@ -62,7 +73,6 @@ function parseArgumentsIntoOptions(rawArgs) {
 }
 
 async function createPackage(options) {
-
   const spinner = ora("Creating Package").start();
   spinner.color = "yellow";
   try {
@@ -76,30 +86,36 @@ async function createPackage(options) {
     spinner.text = "Package Created";
     const stream = fs.createReadStream(destination);
     const formData = new FormData();
-    formData.append('package', stream, {
-        contentType: 'application/zip',
-        filename: "destination.zip",
-      });
+    formData.append("package", stream, {
+      contentType: "application/zip",
+      filename: "destination.zip",
+    });
     spinner.text = "Uploading Package";
     const headers = {
-        Authorization : 'Basic ' + Buffer.from(options.username + ":" + options.password).toString('base64')
+      Authorization:
+        "Basic " +
+        Buffer.from(options.username + ":" + options.password).toString(
+          "base64"
+        ),
     };
-    
+
     const result = await fetch(options.server + "/api/apps/install", {
       method: "POST",
       body: formData,
       headers,
-      redirect: 'follow',
+      redirect: "follow",
       withCredentials: true,
-        credentials: 'include',
+      credentials: "include",
     });
     spinner.stop();
     if (result.status !== 200) {
       console.log(
-        chalk.red.bold("Error installing package: " + result.statusText + result.status)
+        chalk.red.bold(
+          "Error installing package: " + result.statusText + result.status
+        )
       );
       const error = await result.text();
-      console.log(error)
+      console.log(error);
     } else {
       console.log(chalk.green.bold("Package installed"));
     }
@@ -117,13 +133,186 @@ function watchCode(options) {
   });
 }
 
+let soltoken = null;
+
+function login() {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(cors());
+
+  return new Observable(async (observer) => {
+    const p = new Promise((resolve, reject) => {
+      app.post("/ok", async function (req, res) {
+        if (!req.body.token) {
+          reject(new Error("no token returned"));
+        } else {
+          resolve(req.body.token);
+        }
+        res.end("");
+      });
+    });
+
+    observer.next("Connecting OAuth Server");
+    const server = await app.listen(2710);
+    observer.next("Server connected");
+
+    await open("https://beta.soluneo.com/login?cli");
+    observer.next("Login Screen");
+
+    p.then((token) => {
+      observer.next(token);
+      soltoken = token;
+      server.close();
+      observer.complete();
+    }).catch((e) => {
+      server.close();
+      observer.complete();
+    });
+  });
+}
+
+async function download() {
+  const res = await fetch('https://github.com/lowcodecms/development/archive/main.zip');
+  await new Promise((resolve, reject) => {
+    const fileStream = fs.createWriteStream('./lowcodecms.zip');
+    res.body.pipe(fileStream);
+    res.body.on("error", (err) => {
+      reject(err);
+    });
+    fileStream.on("finish", function() {
+      resolve();
+    });
+  });
+}
+
+
+
 export function cli(args) {
-  let options = parseArgumentsIntoOptions(args);
-  const tasks = new Listr([
+// lowcodecms/development-main
+if (args.indexOf("stop") !== -1) {    
+  const tasks = new Listr([      
     {
-      title: "Watch files",
-      task: () => watchCode(options),
+      title: "Stopping development server",
+      task: (ctx, task) => execa(`cd ./lowcodecms/development-main && docker-compose down`, [], {shell: true})        
+      .catch((e) => {
+        console.log(e)
+          task.skip('Could not stop developmentserver');
+      })
     }
   ]);
   tasks.run();
+
+  return;
+
+}
+
+  // lowcodecms/development-main
+  if (args.indexOf("start") !== -1) {    
+    const tasks = new Listr([      
+      {
+        title: "Starting development server",
+        task: (ctx, task) => execa(`cd ./lowcodecms/development-main && docker-compose up -d`, [], {shell: true})        
+        .catch((e) => {
+          console.log(e)
+            task.skip('Could not start developmentserver');
+        })
+      },
+      {
+        title: "Opening Browser on http://localhost/console (superuser / super2020)",
+        task: ()=>open("http://localhost/console")
+      }
+    ]);
+    tasks.run();
+
+    return;
+
+  }
+
+  if (args.indexOf("install") !== -1) {    
+    const tasks = new Listr([
+      {
+        title: "Download Installation Files",
+        task: () => download(),
+      },
+      {
+        title: "Extract Files",
+        task: ()=>{
+          return extract("./lowcodecms.zip", { dir: process.cwd() + "/lowcodecms" })
+        }
+      }
+    ]);
+    tasks.run();
+
+    return;
+  }
+  if (args.indexOf("login") !== -1) {
+    const tasks = new Listr([
+      {
+        title: "Authenticate to LowCode CMS",
+        task: () => login(),
+      },
+      {
+        title: "Store credentials",
+        task: (ctx, task) => {
+          if (!soltoken) {
+            task.skip("token not valid");
+          }          
+        },
+      },
+      {
+        title: "Read Developer Settings",
+        task: async (ctx, task) => {
+          if (!soltoken) {
+            task.skip("token not valid");
+          } else {
+            const url = "https://my.lowcodecms.com/api/settings";
+            const result = await fetch(url, {
+              method: "GET",
+              withCredentials: true,
+              credentials: "include",
+              headers: {
+                Authorization: "Bearer " + soltoken,
+              },
+            });
+            const data = await result.json();
+            ctx.devsettings = {...data.settings};
+          }
+        },
+      },
+      {
+        title: 'Docker inkstalled?',
+        task: (ctx, task) => execa('docker')
+            .catch(() => {
+                ctx.docker = false;
+ 
+                task.skip('Docker not available`');
+            })
+    },
+    {
+      title: 'Authenticate to Container Registry',
+      task: (ctx, task) => execa(`echo ${ctx.devsettings.GithubToken} | docker login ghcr.io -u ${ctx.devsettings.GithubUser} --password-stdin`, [], {shell: true})        
+          .catch((e) => {
+
+            console.log(e)
+              task.skip('Could not authtenticate to registry`' + JSON.stringify(e));
+          })
+  },
+
+      {
+        title: "finishing",
+        task: () => setTimeout(() => process.exit(0), 100),
+      },
+    ]);
+
+    tasks.run();
+  } else {
+    let options = parseArgumentsIntoOptions(args);
+    const tasks = new Listr([
+      {
+        title: "Watch files",
+        task: () => watchCode(options),
+      },
+    ]);
+    tasks.run();
+  }
 }
